07ccf9009a3c44ed5c9452d3514a9142c8a74eae
Sofia-Bastias <75391432+Sofia-Bastias@users.noreply.github.com>
2025-11-02 00:00:05 -0300

    Arreglo pantalla blanca

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import TopBar from "./components/TopBar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
